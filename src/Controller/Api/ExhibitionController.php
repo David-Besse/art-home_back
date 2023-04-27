6185e23bcee42ff9cf7ce36df387dfe460f49d99
MathieuZagar <zagar.mathieu@gmail.com>
2023-04-27 18:33:35 +0200

    delete groups for homepage

--- src/Controller/Api/ExhibitionController.php
+++ src/Controller/Api/ExhibitionController.php
@@ -225,6 +225,7 @@ class ExhibitionController extends AbstractController
         $exhibitionsList = $exhibitionRepository->findby(['status'=>1,'artist'=>$artist]);
 
         // return status 200
-        return $this->json($exhibitionsList, Response::HTTP_OK, [], ['groups' => 'get_exhibitions_title_and_id']);
+        return $this->json($exhibitionsList, Response::HTTP_OK);
     }
+    
 }
