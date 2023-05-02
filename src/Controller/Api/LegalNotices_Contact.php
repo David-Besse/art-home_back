c2133cf097d0134ef75e560f2b712776441740db
Aurelia-Perrier <aurelia.perrier17@gmail.com>
2023-05-02 17:01:01 +0200

    method to get contact and legal notices

---
+++ src/Controller/Api/LegalNotices_Contact.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Repository\ContactRepository;
+use App\Repository\LegalNoticesRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LegalNotices_Contact extends AbstractController
+{
+    /**
+     * Get legal notices
+     *
+     * @param LegalNoticesRepository $legalNoticesRepository
+     * @Route ("/api/legal-notices", name="app_api_legale_notices", methods={"GET"})
+     */
+    public function getLegalNotices(LegalNoticesRepository $legalNoticesRepository) : Response
+    {
+
+        $legalNotices = $legalNoticesRepository->findAll();
+
+        return $this->json(
+            $legalNotices,
+            Response::HTTP_OK,
+            [],
+            ['groups' => 'get_legal_notices']
+        );
+    }
+
+    /**
+     * Get contact
+     *
+     * @param ContactRepository $contactRepository
+     * @Route ("api/contact", name="app_api_contact", methods={"GET"})
+     */
+    public function getContact(ContactRepository $contactRepository)
+    {
+        $contact = $contactRepository->findAll();
+
+        return $this->json(
+            $contact,
+            Response::HTTP_OK,
+            [],
+            ['groups' => 'get_contact']
+        );
+    }
+}
